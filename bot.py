ad814f1ec3c343888d645c00015921768f601ef9
Serhii Kroshka <pm012@ukr.net>
2023-12-13 23:12:23 +0200

    Added initial frame for future bot. Defined needed function. Added description

---
+++ bot.py
@@ -0,0 +1,50 @@
+
+phone_book = dict()
+
+
+# handling errors
+def input_error():
+    pass
+
+
+# Decorator
+@input_error
+def error_handling_factory():
+    pass
+
+
+# Add contact to the data base (command: add)
+def set_contact(contact:str)->str:
+    pass
+
+
+# Get contact phone by name (command: phone)
+def get_phone(name:str)->str:
+    pass
+
+# Update phone for existing contact by its name (command: change)
+def update_phone(name:str, phone:str)->str:
+    pass
+
+
+# Quit the program ( command: good buy, close, exit)
+def quit_bot():
+    pass
+
+
+
+
+
+# Print all contacts in the data base (command: show all)
+def display():
+    pass
+
+
+
+
+
+
+
+
+def main():
+    print()
